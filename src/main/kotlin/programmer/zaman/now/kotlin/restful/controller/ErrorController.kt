297f01d772ab525f31aa2d8d493c9ef8870b7ada
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-03-19 11:48:55 +0700

    Step 6: Validasi request

---
+++ src/main/kotlin/programmer/zaman/now/kotlin/restful/controller/ErrorController.kt
@@ -0,0 +1,18 @@
+package programmer.zaman.now.kotlin.restful.controller
+
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import programmer.zaman.now.kotlin.restful.model.WebResponse
+import javax.validation.ConstraintViolationException
+
+@RestControllerAdvice
+class ErrorController {
+    @ExceptionHandler(value = [ConstraintViolationException::class])
+    fun validationHandler(constraintViolationException: ConstraintViolationException): WebResponse<String> {
+        return WebResponse(
+                code = 400,
+                status = "Bad Request",
+                data = constraintViolationException.message!!
+        )
+    }
+}
--- src/main/kotlin/programmer/zaman/now/kotlin/restful/service/impl/ProductServiceImpl.kt
+++ src/main/kotlin/programmer/zaman/now/kotlin/restful/service/impl/ProductServiceImpl.kt
@@ -6,28 +6,23 @@ import programmer.zaman.now.kotlin.restful.model.CreateProductRequest
 import programmer.zaman.now.kotlin.restful.model.ProductResponse
 import programmer.zaman.now.kotlin.restful.repository.ProductRepository
 import programmer.zaman.now.kotlin.restful.service.ProductService
+import programmer.zaman.now.kotlin.restful.validation.ValidationUtil
 import java.util.*
 
 @Service
-class ProductServiceImpl(val productRepository: ProductRepository) : ProductService {
+class ProductServiceImpl(
+    val productRepository: ProductRepository,
+    val validationUtil: ValidationUtil
+) : ProductService {
     override fun create(createProductRequest: CreateProductRequest): ProductResponse {
-        val product = Product(
-                id = createProductRequest.id,
-                name = createProductRequest.name,
-                price = createProductRequest.price,
-                quantity = createProductRequest.quantity,
-                createdAt = Date(),
-                updatedAt = null
-        )
+        validationUtil.validate(createProductRequest)
+
+        val product = Product(id = createProductRequest.id!!, name = createProductRequest.name!!,
+                price = createProductRequest.price!!, quantity = createProductRequest.quantity!!, createdAt = Date(),
+                updatedAt = null)
         productRepository.save(product)
 
-        return ProductResponse(
-                id = product.id,
-                name = product.name,
-                price = product.price,
-                quantity = product.quantity,
-                createdAt = product.createdAt,
-                updatedAt = product.updatedAt
-        )
+        return ProductResponse(id = product.id, name = product.name, price = product.price, quantity = product.quantity,
+                createdAt = product.createdAt, updatedAt = product.updatedAt)
     }
 }
