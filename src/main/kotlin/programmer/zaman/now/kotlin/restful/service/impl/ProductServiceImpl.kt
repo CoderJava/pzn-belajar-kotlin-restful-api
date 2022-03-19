21b8bc07f8724243d8c2db96cac893e1d18f21d9
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-03-19 12:09:07 +0700

    Reformat code

--- src/main/kotlin/programmer/zaman/now/kotlin/restful/service/impl/ProductServiceImpl.kt
+++ src/main/kotlin/programmer/zaman/now/kotlin/restful/service/impl/ProductServiceImpl.kt
@@ -17,12 +17,23 @@ class ProductServiceImpl(
     override fun create(createProductRequest: CreateProductRequest): ProductResponse {
         validationUtil.validate(createProductRequest)
 
-        val product = Product(id = createProductRequest.id!!, name = createProductRequest.name!!,
-                price = createProductRequest.price!!, quantity = createProductRequest.quantity!!, createdAt = Date(),
-                updatedAt = null)
+        val product = Product(
+                id = createProductRequest.id!!,
+                name = createProductRequest.name!!,
+                price = createProductRequest.price!!,
+                quantity = createProductRequest.quantity!!,
+                createdAt = Date(),
+                updatedAt = null
+        )
         productRepository.save(product)
 
-        return ProductResponse(id = product.id, name = product.name, price = product.price, quantity = product.quantity,
-                createdAt = product.createdAt, updatedAt = product.updatedAt)
+        return ProductResponse(
+                id = product.id,
+                name = product.name,
+                price = product.price,
+                quantity = product.quantity,
+                createdAt = product.createdAt,
+                updatedAt = product.updatedAt
+        )
     }
 }
