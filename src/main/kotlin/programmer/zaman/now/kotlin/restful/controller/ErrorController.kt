3edf1e30f58cbc242f9144f382162f18aeb42e21
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-03-19 12:08:52 +0700

    Improvement: Ubah respon kode dari `ErrorController` agar menjadi 400 Bad Request

--- src/main/kotlin/programmer/zaman/now/kotlin/restful/controller/ErrorController.kt
+++ src/main/kotlin/programmer/zaman/now/kotlin/restful/controller/ErrorController.kt
@@ -1,12 +1,15 @@
 package programmer.zaman.now.kotlin.restful.controller
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import programmer.zaman.now.kotlin.restful.model.WebResponse
 import javax.validation.ConstraintViolationException
 
 @RestControllerAdvice
 class ErrorController {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = [ConstraintViolationException::class])
     fun validationHandler(constraintViolationException: ConstraintViolationException): WebResponse<String> {
         return WebResponse(
